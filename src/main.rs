905c10de3298e9758791e31298716a55dd9fa1ad
Al Madireddy <al.madireddy@gmail.com>
2020-05-09 19:54:37 -0500

    pane manager handles render

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,6 @@ fn main() {
     let (row_count, col_count) = easy.get_row_col_count();
 
     let mut kanban: Kanban = initialize_sample_data();
-    println!("todo: {:?}", kanban.todo);
 
     easy.set_keypad_enabled(true);
     easy.set_input_mode(InputMode::Character);
@@ -59,103 +58,10 @@ fn main() {
     easy.set_color_pair(ColorPair::new(Color::White, Color::Black));
     easy.set_bold(false);
 
-    let mut cur_loc = 0;
-
     let mut pane = PaneManager::new(row_count, col_count);
 
     loop {
-        for i in 0..row_count {
-            easy.move_rc(i, pane.left_divider);
-            easy.print("|");
-            easy.move_rc(i, pane.right_divider);
-            easy.print("|");
-        }
-
-        for i in &kanban.todo {
-            easy.move_rc(cur_loc, 1);
-            easy.print(i.name.as_str());
-            cur_loc += 1;
-        }
-
-        cur_loc = 0;
-
-        for i in &kanban.working {
-            easy.move_rc(cur_loc, pane.left_divider + 2);
-            easy.print(i.name.as_str());
-            cur_loc += 1;
-        }
-
-        cur_loc = 0;
-
-        for i in &kanban.done {
-            easy.move_rc(cur_loc, pane.right_divider + 2);
-            easy.print(i.name.as_str());
-            cur_loc += 1;
-        }
-
-        easy.refresh();
-        let i = easy.get_input();
-
-        match i {
-            Some(input) => {
-                match input {
-                    Input::KeyLeft => {
-                        kanban.todo.push(Item{
-                            name: String::from("Key left pressed")
-                        });
-                    },
-                    Input::KeyRight => {
-                        kanban.working.push(Item{
-                            name: String::from("Key right pressed")
-                        });
-                    },
-                    Input::Character(q) => {
-                        match q {
-                            'q' => {
-                                break
-                            },
-                            _ => {}
-                        }
-                    }
-                    Input::KeyUp => {
-                        let mut inp = String::new();
-                        let mut cur_inp_col = pane.right_divider + 2;
-
-                        loop {
-                            easy.move_rc(1, cur_inp_col);
-                            easy.print(&inp);
-                            easy.move_rc(1, cur_inp_col + inp.len() as i32);
-                            easy.print("_");
-
-                            let input_char = easy.get_input().unwrap();
-                            match input_char {
-                                Input::Character(c) => {
-                                    inp.push(c);
-                                }
-                                Input::KeyUp => {
-                                    for i in inp.chars() {
-                                        easy.delete_line();
-                                    }
-                                    break
-                                }
-                                _ => {
-
-                                }
-                            }
-
-                            easy.move_rc(1, cur_inp_col);
-                        }
-
-                        kanban.todo.push(Item{name: inp});
-                    }
-                    _ => {
-
-                    }
-                }
-            },
-
-            _ => {}
-        }
+        pane.render(&mut easy, &mut kanban);
     }
 
     println!("Quitting");
--- src/pane_manager.rs
+++ src/pane_manager.rs
@@ -1,8 +1,14 @@
+extern crate easycurses;
+use crate::Kanban;
+use crate::Item;
+
+use easycurses::{EasyCurses, Input, CursorVisibility};
+
 pub struct PaneManager {
     pub left_divider: i32,
     pub right_divider: i32,
     pub selected_pane: i32,
-    pub selected_item: i32,
+    pub selected_item: i32
 }
 
 impl PaneManager {
@@ -24,5 +30,90 @@ impl PaneManager {
 
     pub fn set_selected_item(&mut self, s: i32) {
         self.selected_item = s;
+    }
+
+    pub fn render(&mut self, easy: &mut EasyCurses, kanban: &mut Kanban) {
+        let mut cur_loc: i32 = 0;
+        let (row_count, col_count) = easy.get_row_col_count();
+
+        for i in 0..row_count {
+            easy.move_rc(i, self.left_divider);
+            easy.print("|");
+            easy.move_rc(i, self.right_divider);
+            easy.print("|");
+        }
+
+        for i in &kanban.todo {
+            easy.move_rc(cur_loc, 1);
+            easy.print(i.name.as_str());
+            cur_loc += 1;
+        }
+
+        cur_loc = 0;
+
+        for i in &kanban.working {
+            easy.move_rc(cur_loc, self.left_divider + 2);
+            easy.print(i.name.as_str());
+            cur_loc += 1;
+        }
+
+        cur_loc = 0;
+
+        for i in &kanban.done {
+            easy.move_rc(cur_loc, self.right_divider + 2);
+            easy.print(i.name.as_str());
+            cur_loc += 1;
+        }
+
+        easy.refresh();
+        match easy.get_input().unwrap() {
+            Input::KeyLeft => {
+                kanban.todo.push(Item{
+                    name: String::from("Key left pressed")
+                });
+            },
+            Input::KeyRight => {
+                kanban.working.push(Item{
+                    name: String::from("Key right pressed")
+                });
+            },
+            Input::Character(c) => {
+                match c {
+                    'i' => {
+                        let mut inp = String::new();
+                        let mut cur_inp_col = self.right_divider + 2;
+
+                        loop {
+                            easy.move_rc(1, cur_inp_col);
+                            easy.print(&inp);
+                            easy.move_rc(1, cur_inp_col + inp.len() as i32);
+                            easy.print("_");
+
+                            let input_char = easy.get_input().unwrap();
+                            match input_char {
+                                Input::Character('\n')=> {
+                                    for _ in 0..inp.len() {
+                                        easy.delete_line();
+                                    }
+                                    break
+                                }
+                                Input::Character(c) => {
+                                    inp.push(c);
+                                }
+                                _ => {}
+                            }
+
+                            easy.move_rc(1, cur_inp_col);
+                        }
+
+                        kanban.todo.push(Item{name: inp});
+                    },
+                    _ => {}
+                }
+            }
+            _ => {
+
+            }
+        }
     }
 }
